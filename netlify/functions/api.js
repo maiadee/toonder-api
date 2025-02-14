6eed326b15cd3a0520206e3a8b499f546e9da408
Maiadee <maiadeemmony@hotmail.co.uk>
2025-02-14 09:28:01 +0000

    added netlify files

---
+++ netlify/functions/api.js
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose from "mongoose";
+import mongoSanitize from "express-mongo-sanitize";
+import "dotenv/config";
+import serverless from "serverless-http";
+
+import logger from "./middleware/logger.js";
+import errorHandler from "./middleware/errorHandler.js";
+
+import userController from "../../controllers/userController.js";
+import profileController from "../../controllers/profileController.js";
+import messageController from "../../controllers/messageController.js";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(mongoSanitize());
+app.use(logger);
+app.use(cors());
+
+app.use("/", userController);
+app.use("/", profileController);
+app.use("/", messageController);
+
+app.use(errorHandler);
+
+const establishServerConnections = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("👍 Database connection established");
+  } catch (error) {
+    console.log(error);
+  }
+};
+establishServerConnections();
+export const handler = serverless(app);
